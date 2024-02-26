5474650b9ef755ba914ecad0cbdba5c5275757d1
Fauzan Naufan <fauzannaufan@gmail.com>
2024-02-26 23:35:33 +0700

    fix: config

--- config.dev.js
+++ config.dev.js
@@ -1,7 +1,7 @@
 module.exports = exports = {
     devMode: false,
-    prodUrl: "widgets.kominfo.go.id",
-    devUrl: /wbksrc/g,
+    prodUrl: "nartung-dev.layanan.go.id/resources/gpr",
+    devUrl: /localhost:5500\/public\/src/g,
     prodCssFile: 'gpr-widget-kominfo.min.css',
     devCssFile: 'gpr-widget-kominfo.css',
     useHttps: true
