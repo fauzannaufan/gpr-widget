6c4c36b047bf1f920df52bcb6a4770b257c77b38
Fauzan Naufan <fauzannaufan@gmail.com>
2024-03-11 13:30:26 +0700

    chore: keep images

--- config.dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = exports = {
-    devMode: false,
-    prodUrl: "nartung-dev.layanan.go.id/gpr",
-    devUrl: /localhost:5500\/public\/src/g,
-    prodCssFile: 'gpr-widget-kominfo.min.css',
-    devCssFile: 'gpr-widget-kominfo.css',
-    useHttps: true
-};
